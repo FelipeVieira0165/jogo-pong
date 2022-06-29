5cfe20c76bc099f24c8e561f29730915dbd93b82
Felipe Vieira de Oliveira <106687720+FelipeVieira0165@users.noreply.github.com>
2022-06-29 10:53:41 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+//variáveis da bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 15;
+let raio = diametro / 2 ;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+//variáveis da raqute
+let xRaquete = 5;
+let yRaquete = 150;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+} 
+
+function mostraBolinha(){
+  circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentaBolinha(){
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
+
+function verificaColisaoBorda(){
+  if (xBolinha + raio> width ||
+     xBolinha - raio< 0){
+    velocidadeXBolinha *= -1;
+  }
+  if (yBolinha + raio> height ||
+     yBolinha - raio < 0){
+    velocidadeYBolinha *= -1;
+  }
+}
+
+function mostraRaquete(){
+
+
+
+    
+    
+
+
